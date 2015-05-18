4b3facc4d6e6acfcfea341a22e38562a44caf36d
Daniel Manson <d.manson.11@ucl.ac.uk>
2015-05-18 12:55:10 +0100

    bugfix - splice was still not correct

--- cut.js
+++ cut.js
@@ -97,30 +97,47 @@ T.CUT = function(ORG){//class factory
 		return k;
     }
 
+    var DebugState = function(str){
+    	if(str)
+    		console.log(">>" + str);
+    	console.log("groupToImmutablesMapping: [" + Array.prototype.join.call(this._.groupToImmutablesMapping,', ') + "]");
+    }
+
 	var SpliceImmutables = function(a,n_remove /* , inds_1, inds_2, ... */){	//behaves like javascript array "splice"
 		var s = this._.immutablesSlots;
 		var m = this._.groupToImmutablesMapping;
-		
+		DebugState.call(this, "A. splice a,n: " + a + "," +n_remove)
 		var invalidated = M.repvec(0,s.length);
 		
 		// clear the slots with groups  a,a+1,...,a+n_remove-1
 		for(var i=0;i<n_remove;i++)
 			invalidated[DeleteImmutable.call(this,a+i)] = 1;
 		
+		DebugState.call(this, "B. splice a,n: " + a + "," +n_remove)
 		// for all slots corresponding to groups a,a+1,a+2,...,nGroups, push an incremented group number onto the group_history
 		var increment = arguments.length-2 - n_remove; //e.g. if you remove one and add three the increment will be two
-		if(increment) //if increment is zero we don't need to touch the other immutables
-			for(var g=m.length-1;g>=a;g--)if(m[g]){
+		if(increment > 0){
+			for(var g=m.length-1;g>=a;g--)if(m[g]){ // loop in reverse so as not to overwrite as you go
 				s[m[g]].group_history.push(g+increment);
 				invalidated[m[g]] = 1;
 				m[g+increment] = m[g];
 				m[g] = null;
 			}
+		}
+		else if(increment < 0){
+			for(var g=a;g<m.length;g++)if(m[g]){ // loop forward so as not to overwrite as you go
+				s[m[g]].group_history.push(g+increment);
+				invalidated[m[g]] = 1;
+				m[g+increment] = m[g];
+				m[g] = null;
+			}		
+		} //if increment is zero we don't need to touch the other immutables
 		
+		DebugState.call(this, "C. splice a,n: " + a + "," +n_remove)
 		// add the new (inds,group_num) pairs into vacant slots, using group numbers a,a+1,a+2,...
 		for(var i=2;i<arguments.length;i++)
 			invalidated[NewImmutable.call(this,arguments[i],a+i-2)] = 1;
-
+		DebugState.call(this, "D. splice a,n: " + a + "," +n_remove)
 		return invalidated;
 	}
 	
