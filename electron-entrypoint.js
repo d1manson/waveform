fb3353ed73c5503cb052231c631d679f76355862
Daniel Manson <danielmanson.uk@gmail.com>
2018-12-08 18:37:20 +0000

    use older version of electron (not sure if it really helps)

--- electron-entrypoint.js
+++ electron-entrypoint.js
@@ -1,11 +1,26 @@
 const { app, BrowserWindow } = require("electron");
+const path = require("path");
+const url = require("url");
 
 function createWindow() {
   // Create the browser window.
   win = new BrowserWindow({ width: 800, height: 600 });
 
+  win.on("closed", () => {
+    // Dereference the window object, usually you would store windows
+    // in an array if your app supports multi windows, this is the time
+    // when you should delete the corresponding element.
+    win = null;
+  });
+
   // and load the index.html of the app.
-  win.loadFile("index.html");
+  win.loadURL(
+    url.format({
+      pathname: path.join(__dirname, "index.html"),
+      protocol: "file:",
+      slashes: true
+    })
+  );
 }
 
 app.on("ready", createWindow);
